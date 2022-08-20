68cafd41754a6fb421eef585c8dd894edcaff3ad
slot <jamesak.de@gmail.com>
2022-08-20 14:16:00 +0000

    1stinstance

---
+++ push.sh
@@ -0,0 +1,14 @@
+#Names of my favorite artistes
+wizkid
+asake
+olamide
+davido
+fireboy
+oxlade
+victony
+2pac
+biggie
+dr dre
+eminem
+snop doggy dog
+
---
+++ shot.py
@@ -0,0 +1,8 @@
+#List of private universities participating in NUGA
+bells University
+convenant University
+bowen University
+babcock university
+abuad
+crowford university
+redeemers university
